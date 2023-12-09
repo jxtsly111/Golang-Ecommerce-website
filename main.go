ae03c85f3552d3df775417a6ec77f28a07c6f6be
Your Name <jxtsly@gmail.com>
2023-12-09 11:22:16 +0100

    main file

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
 
-func main() {
-    fmt.Println("Hello, World!")
+	"github.com/gin-gonic/gin"
+	"github.com/jxtsly111/ecommerce-yt/controllers"
+	"github.com/jxtsly111/ecommerce-yt/database"
+	"github.com/jxtsly111/ecommerce-yt/middleware"
+	"github.com/jxtsly111/ecommerce-yt/routes"
+)
+
+func main()  {
+    port := os.Getenv("PORT")
+    if port == "" {
+        port = "8000"
+    }
+
+    app := controllers.NewApplication(database.ProductData(database.Client, "Products"),database.UserData(database.Client, "Users"))
+
+    router :=gin.New()
+    router.Use(gin.Logger())
+
+    routes.UserRoutes(router)
+    router.Use(middleware.Authentication())
+
+    router.GET("/addtocart", app.AddToCart())
+    router.GET("/removeitem", app.Removeitem())
+    router.GET("/cartcheckout", app.BuyFromCart())
+    router.GET("/instantbuy", app.InstantBuy())
+
+    log.Fatal(router.Run(":" + port))
+
 }
