6c23b768b095a331d504a409498a3c8063ca589e
Your Name <jxtsly@gmail.com>
2023-12-09 15:46:53 +0100

    controllers

--- models/models.go
+++ models/models.go
@@ -9,9 +9,9 @@ type User struct {
 	ID             primitive.ObjectID `json:"_id" bson:"_id"`
 	First_Name      *string             `json:"first_name" validate:"required,min=2,max=30"`  
 	Last_Name       *string             `json:"last_name" validate:"required,min=2,max=30"`
-	Password       *string             `json:"password" validate:"required,min=6"`
-	Email          *string             `json:"email" validate:"email, required"`
-	Phone          *string             `json:"phone" validate:"required"`
+	Password       *string             `json:"password"   validate:"required,min=6"`
+	Email          *string             `json:"email"      validate:"email, required"`
+	Phone          *string             `json:"phone"     validate:"required"`
 	Token          *string             `json:"token"`
 	Refresh_Token   *string             `json:"refresh_token"`
 	Created_At      time.Time          `json:"created_at"`
