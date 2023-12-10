086d85f9eccd62c82cd8f2ac23753e6385d13b7a
Your Name <jxtsly@gmail.com>
2023-12-10 10:10:40 +0100

    sign up validation

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,6 +1,12 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jxtsly111/ecommerce-yt/models"
+)
 
 func HashPassword(password string) string{
 	
@@ -11,7 +17,22 @@ func VerifyPassword(userPassword string, givenPassword string) (bool, string)  {
 }
 
 func Signup() gin.HandlerFunc{
+	return func (c *gin.Context){
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
 
+		var user models.User
+		if err := c.BindJSON(&user); err!= nil {
+			c.JSON{http.StatusBadRequest, gin.H{"error": err.Error()}}
+			return
+		}
+
+		validationErr := Validate.Struct(user)
+		if validationErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr})
+			return
+		}
+	}
 }
 
 func Login() gin.HandlerFunc{
@@ -27,5 +48,5 @@ func SearchProduct() gin.HandlerFunc{
 }
 
 func SearchProductByQuery() gin.HandlerFunc{
-	
+
 }
