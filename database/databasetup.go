fa4a2c13af88217f56b25ef4bb682e06db99bad8
Your Name <jxtsly@gmail.com>
2023-12-10 09:28:01 +0100

    database set up

---
+++ database/databasetup.go
@@ -0,0 +1,47 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func DBSet() *mongo.Client {
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+
+	if err!= nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeOut(context.Background(), 10*time.second)
+
+	defer cancel()
+
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.TODO(),nil)
+	if err!= nil {
+		log.Println("failed to connect to mongodb: (")
+		return nil
+	}
+
+	fmt.Println("Successfully connected to mongodb")
+	return client
+}
+
+	var Client *mongo.Client =DBSet()
+
+func UserData(client *mongo.Client, collectionName string) *mongo.Collection{
+
+}
+
+func ProductData(client *mongo.Client, collectionName string) *mongo.Collection {
+	
+}
