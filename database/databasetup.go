c17f8b0a2c1fe00def15c43a746edd08a2316629
Your Name <jxtsly@gmail.com>
2023-12-10 09:45:52 +0100

    userdata and productdata function

--- database/databasetup.go
+++ database/databasetup.go
@@ -39,9 +39,11 @@ func DBSet() *mongo.Client {
 	var Client *mongo.Client =DBSet()
 
 func UserData(client *mongo.Client, collectionName string) *mongo.Collection{
-
+	var collection *mongo.Collection = client.Database("Ecommerce").Collection(collectionName)
+	return collection
 }
 
 func ProductData(client *mongo.Client, collectionName string) *mongo.Collection {
-	
+	var productCollection *mongo.Collection = client.Database("Ecommerce").Collection(collectionName)
+	return productCollection
 }
