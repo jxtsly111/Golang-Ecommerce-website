ecb81c67414207804d25a8a54803581a24385ab0
Your Name <jxtsly@gmail.com>
2023-12-12 11:07:05 +0100

    buy from cart

--- controllers/cart.go
+++ controllers/cart.go
@@ -91,7 +91,7 @@ func (app *Application) RemoveItem() gin.HandlerFunc {
 
 		defer cancel()
 
-		err = database.RemoveCartItem(ctx, app.prodCollection, app.userCollection, productID, userQueryID )
+		err = database.RemoveCartItem(ctx, app.prodCollection, app.userCollection, productID, userQueryID)
 
 		if err != nil{
 			c.IndentedJSON(http.StatusInternalServerError,err)
@@ -105,49 +105,29 @@ func GetItemFromCart() gin.HandlerFunc {
 	
 }
 
-func BuyFromCart() gin.HandlerFunc{
+func (app *Application) BuyFromCart() gin.HandlerFunc{
+	return func(c *gin.Context){
+		userQueryID := c.Query("id")
 
+		if userQueryID == "" {
+			log.Panicln("user id is empty")
+			_= c.AbortWithError(http.StatusBadRequest, errors.New("UserID is empty"))
+		}
+
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+
+		defer cancel()
+
+		err := database.BuyItemFromCart(ctx, app.userCollection, userQueryID)
+		if err != nil{
+			c.IndentedJSON(http.StatusInternalServerError, err)
+		}
+		c.IndentedJSON("successfully placed the order")
+	}
 }
 
 func (app *Application) InstantBuy() gin.HandlerFunc{
 	return func (c *gin.Context)  {
-		productQueryID := c.Query("id")
-		if productQueryID == "" {
-			log.Println("product id is empty")
-
-			_ = c.AbortWithError(http.StatusBadRequest, errors.New("product id is empty"))
-			return
-		}
-
-		userQueryID := c.Query("userID")
-		if userQueryID == "" {
-			log.Println("user id is empty")
-			_ = c.AbortWithError(http.StatusBadRequest, errors.New("user id is empty"))
-			return
-		}
-
-		productID , err := primitive.ObjectIDFromHex(productQueryID)
-
-		if err!= nil{
-			log.Println(err)
-			c.AbortWithStatus(http.StatusInternalServerError)
-			return
-		}
-
-		var ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
-
-		defer cancel()
-
-		err = database.AddProductToCart(ctx, app.prodCollection, app.userCollection, productID, userQueryID)
-		if err != nil {
-			c.IndentedJSON(http.StatusInternalServer, err)
-		}
-		c.IndentedJSON(200,"Successfully added to the cart")
-	}
-}
-
-func (app *Application) RemoveItem() gin.HandlerFunc {
-	return func (c*gin.Context)  {
 		productQueryID := c.Query("id")
 		if productQueryID == "" {
 			log.Println("product id is empty")
@@ -183,3 +163,4 @@ func (app *Application) RemoveItem() gin.HandlerFunc {
 		c.IndentedJSON(200, "successfully placed the order")
 	}
 }
+
