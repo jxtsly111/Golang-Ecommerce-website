4443eb799db60de5525d651cc7c50e21ef1bf426
Your Name <jxtsly@gmail.com>
2024-01-05 20:33:18 +0100

    user collection

--- controllers/address.go
+++ controllers/address.go
@@ -77,6 +77,24 @@ func EditHomeAddress() gin.HandlerFunc {
 			c.Abort()
 			return
 		}
+		usert_id, err := primitive.ObjectIDFromHex(user_id)
+	if err != nil {
+		c.IndentedJSON(500, "Internal Server Error",)
+	}
+
+	var ctx, cancel = context.WithTimeout(context.Background(),100*time.Second)
+	defer cancel()
+	filter := bson.D{primitive.E{Key: "_id", Value: usert_id}}
+	update := bson.D{{Key: "$set", Value:bson.D{primitive.E{Key: "address.0.house_name", Value: editaddress.House},{Key: "address.0.street_name", Value: editaddress.Street},{Key:"address.0.city_name", Value: editaddress.City},{Key: "address.0.pincode",Value: editaddress.Pincode}}}}
+	_, err = UserCollection.UpdateOne(ctx,filter,update)
+	if err != nil {
+		c.IndentedJSON(500, "Something went wrong")
+		return
+	}
+	defer cancel()
+	ctx.Done()
+	c.IndentedJSON(200, "successfully updated the home address")
+
 	}
 }
 
@@ -94,16 +112,6 @@ func DeleteAddress() gin.HandlerFunc {
 			c.Abort()
 			return
 	}
-	usert_id, err := primitive.ObjectIDFromHex(user_id)
-	if err != nil {
-		c.IndentedJSON(500, "Internal Server Error",)
-	}
-
-	var ctx, cancel = context.WithTimeout(context.Background(),100*time.Second)
-	defer cancel()
-	filter := bson.D{primitive.E{Key: "_id", Value: usert_id}}
-	update := bson.D{{Key: "$set", Value:bson.D{primitive.E{Key: "address.0.house_name", Value: editaddress.House},{Key: "address.0.street_name", Value: editaddress.Street},{Key:"address.0.city_name", Value: editaddress.City},{Key: "address.0.pincode",Value: editaddress.Pincode}}}}
-
 	addresses := make([]models.Address,0)
 	usert_id, err := primitive.ObjectIDFromHex(user_id)
 	if err != nil {
